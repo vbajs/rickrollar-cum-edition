e48db3b37bfa3fbc4f62b2c3adece8082d0e451a
Yahya Wessam <yahyawessam2002@gmail.com>
2022-08-27 17:39:37 +0200

    Add new feature: Surprise Mode
    Surprise Mode now makes the plugin change src artibute in a 1/50 chance.
    Both patching methods are functions, and depending on
    whether the setting 'SurpriseMe" is enabled or disabled (enabled by
    default), the OnLoad function will execute one of them

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,8 +1,9 @@
-import { dependPersist, setDefaults, SText } from 'cumcord-tools';
+import { dependPersist, setDefaults, SText, SSwitch } from 'cumcord-tools';
 
 setDefaults({
     VIDEO_URL: 'https://cdn.discordapp.com/attachments/858899085159563265/860672912062939166/rickrolledin4k.mp4',
     YT_EMBED_URL: 'https://www.youtube.com/embed/o-YBDTqX_ZU',
+    SurpriseMe: true,
 });
 
 export default dependPersist(() => (
@@ -13,5 +14,8 @@ export default dependPersist(() => (
         <SText k="YT_EMBED_URL">
             Youtube video embed URL to replace all youtube embeds.
         </SText>
+        <SSwitch k="SurpriseMe">
+            Surprise Mode: Makes it trigger only on 1 in a 50 chance; If you disable this, it is recommended to reload afterwards.
+        </SSwitch>
     </>
 ));
--- src/index.js
+++ src/index.js
@@ -8,7 +8,35 @@ const Message = webpackModules.find((m) => m.type && m.type.displayName === 'Mes
 
 const Settings = persist.ghost
 
-export function onLoad() {
+function surprisePatching() {
+    unpatch = patch('type', Message, (number) => {
+        const video = (document.getElementsByTagName('video') && document.querySelectorAll("[class*='video']"));
+        for (let i = 0; i < video.length; i++) {
+            number = Math.floor(Math.random() * 50)
+            if (number === 25) {
+                const element = video[i];
+                const videoLink = element.getAttribute('src');
+                if (videoLink != Settings.VIDEO_URL) {
+                    element.setAttribute('src', Settings.VIDEO_URL);
+                };
+            };
+        };
+
+        const embed = document.getElementsByTagName('iframe');
+        for (let i = 0; i < embed.length; i++) {
+            number = Math.floor(Math.random() * 50)
+            if (number === 25) {
+                const element = embed[i];
+                const embedLink = element.getAttribute('src');
+                if (embedLink.startsWith('https://www.youtube.com') && embedLink != (Settings.YT_EMBED_URL + '?autoplay=1&auto_play=1')) {
+                    element.setAttribute('src', (Settings.YT_EMBED_URL + '?autoplay=1&auto_play=1'))
+                };
+            };
+        };
+    });
+};
+
+function normalPatching() {
     unpatch = patch('type', Message, () => {
         const video = (document.getElementsByTagName('video') && document.querySelectorAll("[class*='video']"));
         for (let i = 0; i < video.length; i++) {
@@ -28,7 +56,16 @@ export function onLoad() {
             };
         };
     });
+};
+
+export async function onLoad() {
+    if (Settings.SurpriseMe = true) {
+        surprisePatching()
+    } else {
+        normalPatching()
+    };
 }
+
 
 export function onUnload() {
     unpatch()
